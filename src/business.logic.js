4552dcbe68eadc22f9589eec95b8e8055384586f
Randel M <Randel.C.Moore@gmail.com>
2020-11-20 13:13:04 -0800

    refactor the output function and add business.logic.js file

---
+++ src/business.logic.js
@@ -0,0 +1,5 @@
+export class CalcVal {
+  function calculate(inputNum, factor) {
+    return inputNum * factor
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
-//import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { ExchangeApi } from './exchange-service';
 
 function outputFun(response) {
-  const newCur = ${newCur};
   if (response.conversion_rates) {
-    $("#outputVal").text(`The exchange rate between ${baseCur} and ${newCur} is ${response.conversion_rates.AUD}.`)
+    $("#outputVal").text("The exchange rate between " + $('#baseCur').val() + " and " + $('#newCur').val() + " is " response.conversion_rates.AUD + ". <br>" + inputNum + " is " + CalcVal.calculate(inputNum, response.conversion_rates.AUD) + " " + $('#newCur').val() + ".");
   } else {
     $("#outputVal").text(`There was an error: ${response}`);
   }
@@ -19,7 +18,7 @@ $(document).ready(function() {
   $("#submitButton").click(function(event) {
     event.preventDefault();
     let baseCur = $("#baseCur").val();
-    let newCur = $("#newCur").val();
+    // let newCur = $("#newCur").val();
     (async function() {
       const response = await ExchangeApi.apiCall(baseCur);
       outputFun(response);
