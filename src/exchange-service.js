a47da7fbf346e12f6f616023934c0995aad8c5b1
Randel M <Randel.C.Moore@gmail.com>
2020-11-20 12:30:46 -0800

    flesh out basic api call in exchange-service.js

--- src/exchange-service.js
+++ src/exchange-service.js
@@ -1,6 +1,13 @@
 export class ExchangeApi {
-
-  static apiCall() {
-    `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCur}`
+  static async apiCall(baseCur) {
+    try {
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCur}`);
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    } catch(error) {
+      return error.message;
+    }
   }
 }
