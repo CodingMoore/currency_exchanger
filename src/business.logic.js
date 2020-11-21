cd6cfb7fc4f4c6ffc2e858059e4bf1b4bb7bc7a7
Randel M <Randel.C.Moore@gmail.com>
2020-11-20 16:46:53 -0800

    create the framework for a pseudo database

--- src/business.logic.js
+++ src/business.logic.js
@@ -1,3 +1,8 @@
 export function calc(inputNum, factor) {
   return parseFloat(inputNum) * parseFloat(factor);
 }
+
+export function dataBase(response) {
+  const curKey = Object.entries(response.conversion_rates);
+  console.log(curKey);
+}
--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { ExchangeApi } from './exchange-service';
-import { calc } from './business.logic.js';
+import { calc, dataBase } from './business.logic.js';
 
 function outputFun(response) {
+  let baseCur = $("#baseCur").val();
+  let newCur = $("#newCur").val();
+  let inputVal = $("#inputVal").val();
   if (response.conversion_rates) {
-    $("#outputVal").html("The exchange rate between " + $('#baseCur').val() + " and " + $('#newCur').val() + " is " + response.conversion_rates.AUD + ". <br><br>" + $("#inputVal").val() + " " + $('#baseCur').val() + " = " + calc($("#inputVal").val(), response.conversion_rates.AUD) + " " + $('#newCur').val() + ".");
-    console.log(response);
+    $("#outputVal").html("The exchange rate between " + baseCur + " and " + newCur + " is " + response.conversion_rates + ". <br><br>" + inputVal + " " + baseCur + " = " + calc(inputVal, response.conversion_rates) + " " + newCur + ".");
   } else {
     if (response["error-type"] === "unsupported-code") {
       $("#outputVal").text("That input currency is not supported or does not exist");
@@ -16,19 +18,15 @@ function outputFun(response) {
     $("#outputVal").text(`There was an error: ${response["error-type"]}`);
     }
   }
-
-  //error output
 }
 
 $(document).ready(function() {
   $("#submitButton").click(function(event) {
     event.preventDefault();
-    let baseCur = $("#baseCur").val();
-    // let newCur = $("#newCur").val();
-    // let inputVal = $("#inputVal").val();
     (async function() {
-      const response = await ExchangeApi.apiCall(baseCur);
+      const response = await ExchangeApi.apiCall($("#baseCur").val());
       outputFun(response);
+      dataBase(response);
     })();
   });
 });
