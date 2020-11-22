ea19bc2e1150d28de10e49715dd0fae30ca98b3b
Randel M <Randel.C.Moore@gmail.com>
2020-11-22 06:01:04 -0800

    add if then logic to reduce api calls

--- src/business.logic.js
+++ src/business.logic.js
@@ -2,7 +2,19 @@ export function calc(inputNum, factor) {
   return parseFloat(inputNum) * parseFloat(factor);
 }
 
-export function dataBase(response) {
-  const curKey = Object.entries(response.conversion_rates);
-  console.log(curKey);
+export static class Database {
+  constructor() {
+    this.curKey = curKey;
+    this.conFactor = conFactor
+  }
+
+  dataBase(response, newCur) {
+    const curKey = Object.entries(response.conversion_rates);
+    for (let i = 0; i < curKey.length; i++) {
+      if (newCur === curKey[i][0]) {
+        let conFactor = curKey[i][1];
+        // return conFactor;
+      }
+    }
+  }
 }
--- src/main.js
+++ src/main.js
@@ -3,14 +3,15 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { ExchangeApi } from './exchange-service';
-import { calc, dataBase } from './business.logic.js';
+import { calc, Database } from './business.logic.js';
 
 function outputFun(response) {
   let baseCur = $("#baseCur").val();
   let newCur = $("#newCur").val();
   let inputVal = $("#inputVal").val();
+  dataBase(response, newCur);
   if (response.conversion_rates) {
-    $("#outputVal").html("The exchange rate between " + baseCur + " and " + newCur + " is " + response.conversion_rates + ". <br><br>" + inputVal + " " + baseCur + " = " + calc(inputVal, response.conversion_rates) + " " + newCur + ".");
+    $("#outputVal").html("The exchange rate between " + baseCur + " and " + newCur + " is " + multFactor + ". <br><br>" + inputVal + " " + baseCur + " = " + calc(inputVal, Database.confactor) + " " + newCur + ".");
   } else {
     if (response["error-type"] === "unsupported-code") {
       $("#outputVal").text("That input currency is not supported or does not exist");
@@ -20,13 +21,27 @@ function outputFun(response) {
   }
 }
 
+// function outputDatabase() {
+//   let newCur = $("#newCur").val();
+//   let multFactor = dataBase(response, newCur);
+//   if (response.conversion_rates) {
+//     $("#outputVal").html("The exchange rate between " + baseCur + " and " + newCur + " is " + multFactor + ". <br><br>" + inputVal + " " + baseCur + " = " + calc(inputVal, multFactor) + " " + newCur + ".");
+// }
+// }
+
 $(document).ready(function() {
+  const baseCurArray = ["placeHolder"];
   $("#submitButton").click(function(event) {
     event.preventDefault();
-    (async function() {
-      const response = await ExchangeApi.apiCall($("#baseCur").val());
+    baseCurArray.push($("#baseCur").val());
+    if (baseCurArray[1] === baseCurArray[0]) {
+      let response = "useDatabase"
       outputFun(response);
-      dataBase(response);
-    })();
+    } else {
+      (async function() {
+        const response = await ExchangeApi.apiCall($("#baseCur").val());
+        outputFun(response);
+      })();
+    }
   });
 });
