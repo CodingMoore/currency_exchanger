a2bd02ce468b1356c94d10392cb00b0ebdecc10c
Randel M <Randel.C.Moore@gmail.com>
2020-11-25 19:57:48 -0800

    add automatic select menu option population

--- src/main.js
+++ src/main.js
@@ -4,6 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { ExchangeApi } from './exchange-service';
 import { calc, Database } from './business.logic.js';
+import { moneyArray } from './money.array.js';
+
+$(window).on("load", function(){
+  for (let i = 0; i < moneyArray.length; i++) {
+    $("#baseCur").append("<option " + " " + "value='" + moneyArray[i][0] + "'>" + moneyArray[i][0] + " - " + moneyArray[i][1] + " - " + moneyArray[i][2] + "</option>");
+    $("#newCur").append("<option " + " " + "value='" + moneyArray[i][0] + "'>" + moneyArray[i][0] + " - " + moneyArray[i][1] + " - " + moneyArray[i][2] + "</option>");
+  }
+});
 
 function outputText() {
   let baseCur = $("#baseCur").val();
@@ -53,24 +61,3 @@ $(document).ready(function() {
     }
   });
 });
-
-
-//   databaseNew(response, newCur);
-//   if (response.conversion_rates) {
-//     $("#outputVal").html("The exchange rate between " + baseCur + " and " + newCur + " is " + Database.confactor + ". <br><br>" + inputVal + " " + baseCur + " = " + calc(inputVal, Database.confactor) + " " + newCur + ".");
-//   } else {
-//     if (response["error-type"] === "unsupported-code") {
-//       $("#outputVal").text("That input currency is not supported or does not exist");
-//     } else {
-//     $("#outputVal").text(`There was an error: ${response["error-type"]}`);
-//     }
-//   }
-// }
-
-// function outputDatabase() {
-//   let newCur = $("#newCur").val();
-//   let multFactor = dataBase(response, newCur);
-//   if (response.conversion_rates) {
-//     $("#outputVal").html("The exchange rate between " + baseCur + " and " + newCur + " is " + multFactor + ". <br><br>" + inputVal + " " + baseCur + " = " + calc(inputVal, multFactor) + " " + newCur + ".");
-// }
-// }
